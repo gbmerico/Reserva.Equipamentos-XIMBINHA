62e3793cd11e2de24633ae8a36036df6c84313e1
gbmerico <gabriel@solucaojundiai.com.br>
2021-10-25 20:06:41 -0300

    Add project files.

---
+++ Reserva.Equipamentos-XIMBINHA/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Reserva.Equipamentos_XIMBINHA
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Reserva.Equipamentos-XIMBINHA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Reserva.Equipamentos_XIMBINHA.MvcApplication" Language="C#" %>
