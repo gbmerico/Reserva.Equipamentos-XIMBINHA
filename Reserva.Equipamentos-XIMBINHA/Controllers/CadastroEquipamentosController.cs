805c84854cd297ff973561d3706a5151836d56ab
gbmerico <gabriel@solucaojundiai.com.br>
2021-10-25 20:57:57 -0300

    Criaçao das telas, equipamentos, usuarios e reservas

---
+++ Reserva.Equipamentos-XIMBINHA/Controllers/CadastroEquipamentosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reserva.Equipamentos_XIMBINHA.Controllers
+{
+    public class CadastroEquipamentosController : Controller
+    {
+        // GET: CadastroEquipamentos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Reserva.Equipamentos-XIMBINHA/Controllers/ReservaEquipamentosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reserva.Equipamentos_XIMBINHA.Controllers
+{
+    public class ReservaEquipamentosController : Controller
+    {
+        // GET: ReservaUsuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
