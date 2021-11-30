1de5d2fa56ec281b09b65ba79f3282cb4e535ca7
gbmerico@gmail.com <gbmerico@gmail.com>
2021-11-29 21:11:39 -0300

    new

---
+++ Reserva.Equipamentos-XIMBINHA/Controllers/ListagemEquipamentosController.cs
@@ -0,0 +1,29 @@
+﻿using Reserva.Equipamentos_XIMBINHA.data;
+using Reserva.Equipamentos_XIMBINHA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reserva.Equipamentos_XIMBINHA.Controllers
+{
+    public class ListagemEquipamentosController : Controller
+    {
+        // GET: ListagemEquipamentos
+
+        public ActionResult Index()
+        {
+            using (var context = new BancoDeDadosContext())
+            {
+                List<CadastroEquipamentoModel> lista = context.CadastroEquipamento.ToList();
+                return View(lista);
+            }              
+        }
+
+        public ActionResult Novo()
+        {
+            return RedirectToAction("Index", "CadastroEquipamentos");
+        }
+    }
+}
